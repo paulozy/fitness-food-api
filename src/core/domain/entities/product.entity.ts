49495a0e59e50cc32d49329b6acc6fe3db9d6602
Paulo Abreu <paulo.santana@luby.software>
2023-06-16 17:05:39 -0300

    feat: create product entity

---
+++ src/core/domain/entities/product.entity.ts
@@ -0,0 +1,59 @@
+interface ProductProps {
+  code: number;
+  status: string;
+  imported_t: string;
+  url: string;
+  creator: string;
+  created_t: number;
+  last_modified_t: number;
+  product_name: string;
+  quantity: string;
+  brands: string;
+  categories: string;
+  labels: string;
+  cities: string;
+  purchase_places: string;
+  stores: string;
+  ingredients_text: string;
+  traces: string;
+  serving_size: string;
+  serving_quantity: number;
+  nutriscore_score: number;
+  nutriscore_grade: string;
+  main_category: string;
+  image_url: string;
+}
+
+export class Product {
+  code: number;
+  status: string;
+  imported_t: string;
+  url: string;
+  creator: string;
+  created_t: number;
+  last_modified_t: number;
+  product_name: string;
+  quantity: string;
+  brands: string;
+  categories: string;
+  labels: string;
+  cities: string;
+  purchase_places: string;
+  stores: string;
+  ingredients_text: string;
+  traces: string;
+  serving_size: string;
+  serving_quantity: number;
+  nutriscore_score: number;
+  nutriscore_grade: string;
+  main_category: string;
+  image_url: string;
+
+  private constructor(props: ProductProps) {
+    Object.assign(this, props);
+  }
+
+  public static create(props: ProductProps): Product {
+    return new Product(props);
+  }
+}
