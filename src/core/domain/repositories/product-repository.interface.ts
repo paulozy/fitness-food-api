c9f01d475df94954e7027d2a3dfef96ddf5c92d9
Paulo Abreu <paulo.santana@luby.software>
2023-06-16 17:25:53 -0300

    feat: add exists method to produc repository interface

--- src/core/domain/repositories/product-repository.interface.ts
+++ src/core/domain/repositories/product-repository.interface.ts
@@ -1,6 +1,7 @@
 import { Product } from '../entities/product.entity';
 
 export abstract class ProductRepositoryInterface {
+  abstract exists(code: number): Promise<boolean>;
   abstract create(product: Product): Promise<void>;
   abstract list(): Promise<Product[]>;
   abstract get(code: number): Promise<Product>;
