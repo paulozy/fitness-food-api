fbcd41e21037fb38a29e418904f8a10c75fb7e13
Paulo Abreu <paulo.santana@luby.software>
2023-06-16 17:54:15 -0300

    feat: add error handling

--- src/core/app/usecases/create-product/create-product.usecase.ts
+++ src/core/app/usecases/create-product/create-product.usecase.ts
@@ -1,21 +1,30 @@
+import { InvalidProductCodeError } from '@core/domain/@errors/invalid-product-code-error';
+import { Product } from '@core/domain/entities/product.entity';
 import { ProductRepositoryInterface } from '@core/domain/repositories/product-repository.interface';
 import { CreateProductDTO } from './create-product.dto';
 
 export class CreateProductUseCase {
   constructor(private readonly productRepository: ProductRepositoryInterface) {}
 
-  async execute(product: CreateProductDTO): Promise<void> {
+  async execute(input: CreateProductDTO): Promise<void> {
     try {
-      const productExists = await this.productRepository.exists(product.code);
+      const productExists = await this.productRepository.exists(input.code);
 
       if (productExists) {
         // send notification that product already exists
         return;
       }
 
+      const product = Product.create(input);
+
       await this.productRepository.create(product);
     } catch (error) {
-      console.error(error);
+      switch (error.constructor) {
+        case InvalidProductCodeError:
+          throw new Error(error.message);
+        default:
+          throw new Error('Unexpected error');
+      }
     }
   }
 }
