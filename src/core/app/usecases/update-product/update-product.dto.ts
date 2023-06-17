1699d93e7f56c288f0a4f22caf3622ed85af2ec9
Paulo Abreu <paulo.santana@luby.software>
2023-06-17 00:26:24 -0300

    feat: create update product usecase

---
+++ src/core/app/usecases/update-product/update-product.dto.ts
@@ -0,0 +1,21 @@
+export interface UpdateProductDTO {
+  status?: string;
+  url?: string;
+  brands?: string;
+  categories?: string;
+  labels?: string;
+  cities?: string;
+  purchase_places?: string;
+  stores?: string;
+  ingredients_text?: string;
+  traces?: string;
+  serving_size?: string;
+  serving_quantity?: number;
+  nutriscore_score?: number;
+  nutriscore_grade?: string;
+  main_category?: string;
+  image_url?: string;
+  product_name?: string;
+  quantity?: string;
+  creator?: string;
+}
---
+++ src/core/app/usecases/update-product/update-product.usecase.ts
@@ -0,0 +1,31 @@
+import { ProductRepositoryInterface } from '@core/domain/repositories/product-repository.interface';
+import { ProductNotFoundError } from '../@erros/product-not-found-error';
+import { UpdateProductDTO } from './update-product.dto';
+
+export class UpdateProductUseCase {
+  constructor(private readonly productRepository: ProductRepositoryInterface) {}
+
+  async execute(code: number, data: UpdateProductDTO): Promise<void> {
+    try {
+      const productExists = await this.productRepository.exists(code);
+
+      if (!productExists) {
+        throw new ProductNotFoundError(code);
+      }
+
+      const product = await this.productRepository.get(code);
+
+      product.update(data);
+
+      await this.productRepository.save(product);
+    } catch (error) {
+      console.error(error);
+      switch (error.constructor) {
+        case ProductNotFoundError:
+          throw error;
+        default:
+          throw new Error('Unexpected error');
+      }
+    }
+  }
+}
