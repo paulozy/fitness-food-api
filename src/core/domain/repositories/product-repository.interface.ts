ce9e579a1329e8d66bc20b10f540f45403a6485b
Paulo Abreu <paulo.santana@luby.software>
2023-06-16 23:23:57 -0300

    feat: add params to list products method

--- src/core/domain/repositories/product-repository.interface.ts
+++ src/core/domain/repositories/product-repository.interface.ts
@@ -1,9 +1,14 @@
 import { Product } from '../entities/product.entity';
 
+export interface ListProductsInput {
+  page: number;
+  limit: number;
+}
+
 export abstract class ProductRepositoryInterface {
   abstract exists(code: number): Promise<boolean>;
   abstract create(product: Product): Promise<void>;
-  abstract list(): Promise<Product[]>;
+  abstract list(data: ListProductsInput): Promise<Product[]>;
   abstract get(code: number): Promise<Product>;
   abstract save(product: Product): Promise<void>;
 }
