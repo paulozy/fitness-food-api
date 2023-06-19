3ac704f99b8167f3d60674f0d0e09f6ef9f86a8c
Paulo Abreu <paulo.santana@luby.software>
2023-06-19 18:13:57 -0300

    refactor: improve imports

--- src/core/infra/services/sync-products.module.ts
+++ src/core/infra/services/sync-products.module.ts
@@ -1,17 +1,18 @@
 import { ImportUseCasesFactory } from '@core/app/factories/import-usecases.factory';
+import { ProductUseCasesFactory } from '@core/app/factories/product-usecases.factory';
 import { CreateImportUseCase } from '@core/app/usecases/create-import/create-import.usecase';
 import { CreateProductUseCase } from '@core/app/usecases/create-product/create-product.usecase';
-import { UseCasesFactory } from '@core/utils/usecases-factory';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { PrismaService } from '../database/prisma/prisma.service';
-import { PrismaImportRepository } from '../database/prisma/repositories/prisma-imports-repository';
-import { prismaProductRepository } from '../http/http.module';
+import {
+  prismaImportRepository,
+  prismaProductRepository,
+} from '../http/http.module';
 import { SyncProductsService } from './sync-products.service';
 
-const { createProduct } = UseCasesFactory.create(prismaProductRepository);
-
-const prismaImportRepository = new PrismaImportRepository(new PrismaService());
+const { createProduct } = ProductUseCasesFactory.create(
+  prismaProductRepository,
+);
 const { createImport } = ImportUseCasesFactory.create(prismaImportRepository);
 
 @Module({
