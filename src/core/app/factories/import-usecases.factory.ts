dd52fc4fc46c35a24ba42104bdc6160d90834cb1
Paulo Abreu <paulo.santana@luby.software>
2023-06-19 18:12:41 -0300

    refactor: change usecase from listImports

--- src/core/app/factories/import-usecases.factory.ts
+++ src/core/app/factories/import-usecases.factory.ts
@@ -1,10 +1,11 @@
 import { ImportRepositoryInterface } from '@core/domain/repositories/import-repository.interface';
 import { CreateImportUseCase } from '../usecases/create-import/create-import.usecase';
+import { ListImportsUseCase } from '../usecases/list-imports/list-imports.usecase';
 
 export class ImportUseCasesFactory {
   static create(importRepository: ImportRepositoryInterface) {
     const createImport = new CreateImportUseCase(importRepository);
-    const listImports = new CreateImportUseCase(importRepository);
+    const listImports = new ListImportsUseCase(importRepository);
 
     return {
       createImport,
