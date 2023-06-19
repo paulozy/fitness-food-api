c082e6001fb96ff651d33660b3b2fb6bd4620955
Paulo Abreu <paulo.santana@luby.software>
2023-06-19 17:17:20 -0300

    feat: add types on import repository iterface

--- src/core/domain/repositories/import-repository.interface.ts
+++ src/core/domain/repositories/import-repository.interface.ts
@@ -1,3 +1,21 @@
+import { Import } from '../entities/import.entity';
+
+export interface ListImportsInput {
+  page: number;
+  limit: number;
+}
+
+export interface ListImportsOutput {
+  data: Import[];
+  pagination: {
+    page: number;
+    total: number;
+    totalPages: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+}
+
 export abstract class ImportRepositoryInterface {
-  abstract list(): Promise<any>;
+  abstract list(input: ListImportsInput): Promise<ListImportsOutput>;
 }
