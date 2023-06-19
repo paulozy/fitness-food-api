55f1d8710a5b7fd0aa6e239217f2271cd65bc81c
Paulo Abreu <paulo.santana@luby.software>
2023-06-19 17:57:32 -0300

    feat: add create import usecase to sync-products module

--- src/core/infra/services/sync-products.module.ts
+++ src/core/infra/services/sync-products.module.ts
@@ -1,11 +1,18 @@
+import { ImportUseCasesFactory } from '@core/app/factories/import-usecases.factory';
+import { CreateImportUseCase } from '@core/app/usecases/create-import/create-import.usecase';
 import { CreateProductUseCase } from '@core/app/usecases/create-product/create-product.usecase';
 import { UseCasesFactory } from '@core/utils/usecases-factory';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { PrismaService } from '../database/prisma/prisma.service';
+import { PrismaImportRepository } from '../database/prisma/repositories/prisma-imports-repository';
 import { prismaProductRepository } from '../http/http.module';
 import { SyncProductsService } from './sync-products.service';
 
 const { createProduct } = UseCasesFactory.create(prismaProductRepository);
+
+const prismaImportRepository = new PrismaImportRepository(new PrismaService());
+const { createImport } = ImportUseCasesFactory.create(prismaImportRepository);
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +21,10 @@ const { createProduct } = UseCasesFactory.create(prismaProductRepository);
     {
       provide: CreateProductUseCase,
       useValue: createProduct,
+    },
+    {
+      provide: CreateImportUseCase,
+      useValue: createImport,
     },
   ],
 })
