19fcfc6b6aafcc56a2e4e9b9216bed5ba18604e6
Paulo Abreu <paulo.santana@luby.software>
2023-06-19 10:51:57 -0300

    feat: add route to get a product by code

--- src/core/infra/http/controllers/product/product.controller.ts
+++ src/core/infra/http/controllers/product/product.controller.ts
@@ -1,9 +1,20 @@
+import { GetProductUseCase } from '@core/app/usecases/get-product/get-product.usecase';
 import { ListProductsUseCase } from '@core/app/usecases/list-products/list-products.usecase';
-import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly listProducts: ListProductsUseCase) {}
+  constructor(
+    private readonly listProducts: ListProductsUseCase,
+    private readonly getProduct: GetProductUseCase,
+  ) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
@@ -16,5 +27,13 @@ export class ProductController {
     });
 
     return products;
+  }
+
+  @Get('/:code')
+  @HttpCode(HttpStatus.OK)
+  async get(@Param('code') code: string) {
+    const product = await this.getProduct.execute(code);
+
+    return product;
   }
 }
