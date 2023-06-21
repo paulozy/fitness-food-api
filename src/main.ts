7290ee31e8264f497c9e6c5813195f2cec6eadb4
Paulo Abreu <paulo.santana@luby.software>
2023-06-21 01:12:23 -0300

    feat: config global pipe validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './configs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   setupSwagger(app);
 
