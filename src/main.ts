ca70824780bf5e61127abdcf2feaef386702c60e
Paulo Abreu <paulo.santana@luby.software>
2023-06-21 00:52:14 -0300

    refactor: move swagger setup to a proper file

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  DocumentBuilder,
-  SwaggerDocumentOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { setupSwagger } from './configs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Fitness Food API')
-    .setDescription('This api consists of fitness food products details')
-    .setVersion('1.0')
-    .addTag('fitness-food')
-    .addApiKey({
-      type: 'apiKey',
-      name: 'x-api-key',
-      in: 'header',
-    })
-    .build();
-
-  const options: SwaggerDocumentOptions = {
-    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
-  };
-
-  const document = SwaggerModule.createDocument(app, config, options);
-  SwaggerModule.setup('api', app, document);
+  setupSwagger(app);
 
   await app.listen(3000);
 }
