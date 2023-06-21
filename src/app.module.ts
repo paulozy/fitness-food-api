933dfcd6f135c8ba120687edc15e95434004be09
Paulo Abreu <paulo.santana@luby.software>
2023-06-21 01:10:34 -0300

    feat: add rate limit with throttler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { HttpModule } from '@core/infra/http/http.module';
 import { SyncProductsModule } from '@core/infra/services/sync-products.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ScheduleModule.forRoot(),
     SyncProductsModule,
     ConfigModule.forRoot({
@@ -15,7 +21,11 @@ import { ScheduleModule } from '@nestjs/schedule';
     HttpModule,
     DatabaseModule,
   ],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
