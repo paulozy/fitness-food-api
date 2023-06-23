79c7f6a74c41836adc0eee1c0bc85f9e20a1b6eb
Paulo Abreu <paulo.santana@luby.software>
2023-06-22 23:25:39 -0300

    refactor: improve app module removin unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,17 @@
 import { DatabaseModule } from '@core/infra/database/database.module';
 import { HttpModule } from '@core/infra/http/http.module';
-import { SyncProductsModule } from '@core/infra/services/sync-products.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    DatabaseModule,
+    HttpModule,
     ScheduleModule.forRoot(),
-    SyncProductsModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    HttpModule,
-    DatabaseModule,
   ],
   controllers: [],
   providers: [],
