b0055f675f03d7e621f4b6087ec78093e2cdbcaa
Paulo Abreu <paulo.santana@luby.software>
2023-06-23 01:21:58 -0300

    test: ensure thats be possible create many products

---
+++ src/core/app/usecases/create-many-products/create-many-products.usecase.spec.ts
@@ -0,0 +1,27 @@
+import { createManyProductsPayloadFactory } from '@core/app/factories/create-product.factory';
+import { ProductRepositoryInterface } from '@core/domain/repositories/product-repository.interface';
+import { InMemoryProductRepository } from '@test/repositories/in-memory-product-repository';
+import { CreateManyProductsUseCase } from './create-many-products.usecase';
+
+describe('Create Many Products UseCase', () => {
+  let createManyProductsUseCase: CreateManyProductsUseCase;
+  let productRepository: ProductRepositoryInterface;
+
+  beforeEach(() => {
+    productRepository = new InMemoryProductRepository();
+
+    createManyProductsUseCase = new CreateManyProductsUseCase(
+      productRepository,
+    );
+  });
+
+  it('should be possible create many products', async () => {
+    const payloads = createManyProductsPayloadFactory(100);
+
+    await createManyProductsUseCase.execute({
+      products: payloads,
+    });
+
+    expect(productRepository['products']).toHaveLength(100);
+  });
+});
